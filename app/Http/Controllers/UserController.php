37939733c9d27de18995e7cf8b3e7a52856906d2
rahim8050 <rahimranxx8050@gmail.com>
2025-05-04 13:12:43 +0100

    signup

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+
+class UserController extends Controller
+{
+    //
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'name' => ['required','min:3'],
+            'email' => ['required', 'email',Rule::unique('users', 'email')],
+            'password' => ['required', 'min:8','confirmed']
+        ]);
+        $formFields['password'] = bcrypt($formFields['password']);
+        $user = User::create($formFields);
+        Auth::login($user);
+        return redirect('/')->with('message', 'User Created successfully and logged in');
+    }
+}
